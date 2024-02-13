f85d97adc46b4b096e56bf7f5f1783604281eb74
Luis Martinez <luis_mmartinez@live.com>
2024-02-12 23:39:33 -0500

    Favicon
    -Add favicon image
    -Image not loading
    -Remove usopported property in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   images: {
     domains: ['github.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-        // port: '',
-        // pathname: '/my-bucket/**',
-      },
-    ],
   },
 };
